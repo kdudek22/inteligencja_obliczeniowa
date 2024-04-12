99852b7e8c501315a9581a5bd94a083f22e68418
omnibus1 <appleofeden8686@gmail.com>
2024-04-12 15:20:19 +0200

    finished taxi

--- lab3/main.py
+++ lab3/main.py
@@ -4,70 +4,93 @@ import matplotlib.pyplot as plt
 import pickle
 
 
+class Taxi:
+    def __init__(self, is_training=True, render=False, episodes=1000, discount_factor=0.9, learning_rate=0.06):
+        self.is_training = is_training
+        self.render = render
+        self.episodes = episodes
+        self.discount_factor = discount_factor
+        self.learning_rate = learning_rate
+        self.rewards_for_episode = np.zeros(episodes)
+        self.chances = np.zeros(episodes)
+        self.rng = np.random.default_rng()
+        self.env = gym.make("Taxi-v3", render_mode="human" if self.render else None)
+        self.q = self.get_or_create_q_table()
+        self.run()
 
-def run(episodes, is_training=True, render=False):
-
-    env = gym.make('Taxi-v3', render_mode='human' if render else None)
-
-    if is_training:
-        q = np.zeros((env.observation_space.n, env.action_space.n)) # init a 64 x 4 array
-    else:
-        f = open("taxi.pkl", 'rb')
-        q = pickle.load(f)
-        f.close()
-
-    discount_factor_g = 0.5
-    epsilon = 1
-    epsilon_decay_rate = 0.0001  # epsilon decay rate. 1/0.0001 = 10,000
-    rng = np.random.default_rng()
-    learning_rate_a = 0.4
-    rewards_per_episode = np.zeros(episodes)
-
-    for i in range(episodes):
-        state = env.reset()[0]  # states: 0 to 63, 0=top left corner,63=bottom right corner
-        terminated = False      # True when fall in hole or reached goal
-        truncated = False       # True when actions > 200
-        reward = 0
-
-        while not terminated and not truncated:
-            if is_training and rng.random() < epsilon:
-                action = env.action_space.sample()
-            else:
-                action = np.argmax(q[state, :])
-
-            new_state, reward, terminated, truncated, info = env.step(action)
-
-            if is_training:
-                q[state, action] = q[state, action] + learning_rate_a * (float(reward) + discount_factor_g * np.max(q[new_state, :]) - q[state, action])
-
-            state = new_state
-
-        epsilon = max(epsilon - epsilon_decay_rate, 0)
-
-        if epsilon == 0:
-            learning_rate_a = 0.0005
-
-        rewards_per_episode[i] = reward
-
-    env.close()
-
-    sum_rewards = np.zeros(episodes)
-    for t in range(episodes):
-        lower_idx = max(0, t-100)
-        upper_idx = t+1
-        diff = upper_idx - lower_idx
-        if diff != 101:
-            sum_rewards[t] = np.sum(rewards_per_episode[lower_idx:upper_idx]) * 100/diff
+    def get_or_create_q_table(self):
+        if self.is_training:
+            return np.zeros((self.env.observation_space.n, self.env.action_space.n))
         else:
-            sum_rewards[t] = np.sum(rewards_per_episode[max(0, t-100):(t+1)])
+            with open("q-taxi.pkl", "rb") as f:
+                q = pickle.load(f)
+            return q
 
-    plt.plot(sum_rewards)
-    plt.savefig('taxi.png')
+    def run(self):
+        for i in range(self.episodes):
+            state, info = self.env.reset()
+            terminated, truncated = False, False
 
-    if is_training:
-        f = open("taxi.pkl", "wb")
-        pickle.dump(q, f)
-        f.close()
+            while not terminated and not truncated:
+                action = self.get_next_action(i, state)
+
+                new_state, reward, terminated, truncated, info = self.env.step(action)
+
+                if self.is_training:
+                    self.update_q_table(state, new_state, reward, action)
+
+                state = new_state
+
+                self.rewards_for_episode[i] = reward
+
+        self.env.close()
+
+        if self.is_training:
+            self.save_q_table()
+
+        self.plot_resoults()
+
+    def get_next_action(self, i, state):
+        if not self.is_training:
+            return np.argmax(self.q[state, :])
+        else:
+            episode_to_zero_to_one = ((np.log(1 + i))/ np.log(1 + self.episodes))**2
+            self.chances[i] = episode_to_zero_to_one
+            if self.rng.random() < episode_to_zero_to_one:
+                return np.argmax(self.q[state, :])
+            else:
+                return self.env.action_space.sample()
+
+    def update_q_table(self, state, new_state, reward, action):
+        self.q[state, action] = (1 - self.learning_rate) * self.q[state, action] + self.learning_rate * (
+                float(reward) + self.discount_factor * np.max(self.q[new_state, :]))
+
+    def save_q_table(self):
+        with open("q-taxi.pkl", "wb") as f:
+            pickle.dump(self.q, f)
+
+    def plot_resoults(self):
+        sum_rewards = np.zeros(self.episodes)
+        for t in range(self.episodes):
+            lower_idx = max(0, t - 100)
+            upper_idx = t + 1
+            diff = upper_idx - lower_idx
+            if diff != 101:
+                sum_rewards[t] = (np.sum(self.rewards_for_episode[lower_idx:upper_idx]) * 100 / diff)/100
+            else:
+                sum_rewards[t] = (np.sum(self.rewards_for_episode[max(0, t - 100):(t + 1)]))/100
+
+        plt.plot(sum_rewards)
+        plt.savefig('taxi.png')
+        plt.show()
+
+        plt.plot(self.chances)
+        plt.savefig("odds.png")
+
+
+
 
 if __name__ == '__main__':
-    run(5000, is_training=True, render=True)
+
+    t = Taxi(render=True)
+
